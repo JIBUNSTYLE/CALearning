3b1896e5d02681b8b908dc1faa48ee00ffaeefb0
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2022-01-25 10:01:00 +0900

    Initial Commit

---
+++ CALearning/CALearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CALearningApp.swift
+//  CALearning
+//
+//  Created by 斉藤 祐輔 on 2022/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CALearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CALearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CALearning
+//
+//  Created by 斉藤 祐輔 on 2022/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
