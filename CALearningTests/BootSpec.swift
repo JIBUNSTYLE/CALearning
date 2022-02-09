3135d25c5ec81b0c82872fbf55684fc52d7b0f18
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2022-02-09 13:48:45 +0900

    [wip] mockApiClientでのテスト

--- CALearningTests/BootSpec.swift
+++ CALearningTests/BootSpec.swift
@@ -17,9 +17,40 @@ class BootSpec: QuickSpec {
         let presenter = Presenter()
         
         describe("アプリを起動する") {
+            context("UDIDがない場合") {
+                beforeEach {
+                    presenter.currentView = .splash
+                    Application().discardUdid()
+                }
+                it("アプリはUDIDを取得する") {
+                    
+                    waitUntil(timeout: .seconds(10)) { done in
+                        
+                        let mockApiClient = MockApiClient<Apis.Udid>(
+                            stub: .success(entity: Apis.Udid.Entity(udid: "fugafuga"))
+                            , afterCall: { result in
+                                print("● afterCall: \(result)")
+                                
+                                if case .success(let entity) = result {
+                                    expect(entity.udid).to(equal("fugafuga"))
+                                } else if case .failure(_) = result {
+                                    fail()
+                                }
+                                
+                                done()
+                            })
+                        
+                        let backend = ApiBackend(apiClient: mockApiClient)
+                        Dependencies.shared.set(backend: backend)
+                        
+                        presenter.boot()
+                    }
+                }
+            }
             context("チュートリアル完了の記録がある場合") {
                 beforeEach {
                     presenter.currentView = .splash
+                    Application().save(udid: "hogehoge")
                     Application().hasCompletedTutorial = true
                 }
                 it("アプリはログイン画面を表示") {
@@ -33,6 +64,7 @@ class BootSpec: QuickSpec {
             context("チュートリアル完了の記録がない場合") {
                 beforeEach {
                     presenter.currentView = .splash
+                    Application().save(udid: "hogehoge")
                     Application().hasCompletedTutorial = false
                 }
                 it("アプリはチュートリアル画面を表示") {
